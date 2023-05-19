cb503fe0a41ced0f8fe73bf5dacd6d589683b2cd
lamputtha <131886359+lamputtha@users.noreply.github.com>
2023-05-19 11:08:11 +0200

    Update game-computer.js
    Script fix 6

--- game-computer.js
+++ game-computer.js
@@ -1,37 +1,27 @@
-// Constants for the game choices
-const choices = [
-  { name: "rock", emoji: "✊" },
-  { name: "paper", emoji: "📄" },
-  { name: "scissors", emoji: "✂️" },
-  { name: "lizard", emoji: "🦎" },
-  { name: "spock", emoji: "🖖" }
-];
-
-// Player and computer scores
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 let playerScore = 0;
 let computerScore = 0;
 
-// Function to generate computer's choice
-function getComputerChoice() {
-  const randomIndex = Math.floor(Math.random() * choices.length);
-  return choices[randomIndex].name;
+function playerChoice(choice) {
+  const computerChoice = getComputerChoice();
+  const result = getResult(choice, computerChoice);
+
+  displayChoices(choice, computerChoice);
+  updateScore(result);
+
+  if (playerScore >= 5 || computerScore >= 5) {
+    endGame();
+  }
 }
 
-// Function to update the game result
-function updateResult(playerChoice, computerChoice) {
-  const resultText = document.getElementById("result-text");
-  const playerChoiceElement = document.getElementById("player-choice");
-  const computerChoiceElement = document.getElementById("computer-choice");
+function getComputerChoice() {
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
 
-  const playerEmoji = choices.find(choice => choice.name === playerChoice).emoji;
-  const computerEmoji = choices.find(choice => choice.name === computerChoice).emoji;
-
-  playerChoiceElement.innerHTML = playerEmoji;
-  computerChoiceElement.innerHTML = computerEmoji;
-
-  // Determine the winner
+function getResult(playerChoice, computerChoice) {
   if (playerChoice === computerChoice) {
-    resultText.textContent = "It's a tie!";
+    return "tie";
   } else if (
     (playerChoice === "rock" && (computerChoice === "scissors" || computerChoice === "lizard")) ||
     (playerChoice === "paper" && (computerChoice === "rock" || computerChoice === "spock")) ||
@@ -39,69 +29,78 @@ function updateResult(playerChoice, computerChoice) {
     (playerChoice === "lizard" && (computerChoice === "paper" || computerChoice === "spock")) ||
     (playerChoice === "spock" && (computerChoice === "rock" || computerChoice === "scissors"))
   ) {
-    resultText.textContent = "You win!";
-    playerScore++;
+    return "win";
   } else {
-    resultText.textContent = "Computer wins!";
-    computerScore++;
-  }
-
-  // Update the score
-  updateScore();
-
-  // Check if any player has reached the winning score of 5
-  if (playerScore === 5) {
-    endGame("You win the game!");
-  } else if (computerScore === 5) {
-    endGame("Computer wins the game!");
+    return "lose";
   }
 }
 
-// Function to update the score
-function updateScore() {
+function displayChoices(playerChoice, computerChoice) {
+  const playerChoiceElement = document.getElementById("player-choice");
+  const computerChoiceElement = document.getElementById("computer-choice");
+
+  playerChoiceElement.textContent = getEmoji(playerChoice);
+  computerChoiceElement.textContent = getEmoji(computerChoice);
+}
+
+function getEmoji(choice) {
+  switch (choice) {
+    case "rock":
+      return "🪨";
+    case "paper":
+      return "📄";
+    case "scissors":
+      return "✂️";
+    case "lizard":
+      return "🦎";
+    case "spock":
+      return "🖖";
+    default:
+      return "";
+  }
+}
+
+function updateScore(result) {
   const playerScoreElement = document.getElementById("player-score");
   const computerScoreElement = document.getElementById("computer-score");
+  const resultTextElement = document.getElementById("result-text");
+
+  if (result === "win") {
+    playerScore++;
+    resultTextElement.textContent = "You Win!";
+  } else if (result === "lose") {
+    computerScore++;
+    resultTextElement.textContent = "Computer Wins!";
+  } else {
+    resultTextElement.textContent = "It's a Tie!";
+  }
 
   playerScoreElement.textContent = playerScore;
   computerScoreElement.textContent = computerScore;
 }
 
-// Function to end the game and display the final result
-function endGame(message) {
-  const resultText = document.getElementById("result-text");
+function endGame() {
   const playAgainButton = document.getElementById("play-again");
-  const choices = document.getElementsByClassName("choice");
-
-  resultText.textContent = message;
-  playAgainButton.style.display = "block";
-
-  // Disable the game choices
-  for (let i = 0; i < choices.length; i++) {
-    choices[i].disabled = true;
-  }
+  playAgainButton.textContent = "Reset";
+  playAgainButton.onclick = resetGame;
 }
 
-// Function to reset the game
 function resetGame() {
   playerScore = 0;
   computerScore = 0;
 
-  updateScore();
-
-  const resultText = document.getElementById("result-text");
+  const playerScoreElement = document.getElementById("player-score");
+  const computerScoreElement = document.getElementById("computer-score");
+  const resultTextElement = document.getElementById("result-text");
   const playAgainButton = document.getElementById("play-again");
-  const choices = document.getElementsByClassName("choice");
 
-  resultText.textContent = "";
-  playAgainButton.style.display = "none";
-
-  // Enable the game choices
-  for (let i = 0; i < choices.length; i++) {
-    choices[i].disabled = false;
-  }
+  playerScoreElement.textContent = playerScore;
+  computerScoreElement.textContent = computerScore;
+  resultTextElement.textContent = "";
+  playAgainButton.textContent = "Play Again";
+  playAgainButton.onclick = null;
 }
 
-// Function to go back to the main menu
 function backToMenu() {
   location.href = "index.html";
 }
